ceeaf31cc5285769592c7026efbe5002fd474627
Kevin Wang <126712357+KevinWang676@users.noreply.github.com>
2025-03-04 17:41:12 -0600

    Update infer_stage1.py

--- src/yue/infer_stage1.py
+++ src/yue/infer_stage1.py
@@ -62,7 +62,7 @@ def encode_audio(codec_model, audio_prompt, device, target_bw=0.5):
 
 class Stage1Pipeline:
 
-    def __init__(self, device: torch.device, basic_model_config: str, resume_path: str, seed: int, resume_after_n: int, extend_mp3: str, extend_mp3_start_time: int, extend_mp3_end_time: int, extend_current_segment: bool):
+    def __init__(self, device: torch.device, basic_model_config: str, resume_path: str, seed: int, resume_after_n: int, extend_mp3: str, extend_mp3_end_time: int, extend_current_segment: bool):
         self.device = device
         self.codec_tool = CodecManipulator("xcodec", 0, 1)
         self.basic_model_config = basic_model_config
@@ -196,7 +196,6 @@ class Stage1Pipeline:
         self,
         vocal_path: str,
         instrumental_path: str,
-        extend_mp3_start_time: int,
         extend_mp3_end_time: int,
     ) -> List[int]:
         """Encode dual-track audio into interleaved tokens, with trimming."""
@@ -221,11 +220,9 @@ class Stage1Pipeline:
             for tok in pair
         ]
         
-        if (extend_mp3_start_time > 0 or extend_mp3_end_time > 0):
-            if extend_mp3_end_time == 0:
-                extend_mp3_end_time = 999
-            interleaved = interleaved[int(extend_mp3_start_time * 50 * 2): int(extend_mp3_end_time * 50 * 2)]
-            print("trimmed extend_mp3_interleaved to "+str(extend_mp3_start_time)+"-"+str(extend_mp3_end_time)+" s, size:"+str(len(interleaved))+" tokens")
+        if (extend_mp3_end_time > 0):
+            interleaved = interleaved[: int(extend_mp3_end_time * 50 * 2)]
+            print("trimmed extend_mp3_interleaved up to "+str(extend_mp3_end_time)+"s, size:"+str(len(interleaved))+" tokens")
 
         return interleaved    
 
@@ -336,7 +333,7 @@ class Stage1Pipeline_HF(Stage1Pipeline):
 
 class Stage1Pipeline_EXL2(Stage1Pipeline):
 
-    def __init__(self, model_path: str, device: torch.device, cache_size: int, cache_mode: str, no_flash_attn: bool, **kwargs):
+    def __init__(self, model_path: str, device: torch.device, cache_size: int, cache_mode: str, **kwargs):
         super().__init__(device, **kwargs)
 
         assert device != "cpu", "ExLlamaV2 does not support CPU inference."
@@ -347,8 +344,6 @@ class Stage1Pipeline_EXL2(Stage1Pipeline):
         gpu_split[device_idx] = 9999
         exl2_config = ExLlamaV2Config(model_path)
         exl2_config.no_sdpa = True  # TODO: Figure out why SDPA slows to a crawl when given custom attn mask
-        if no_flash_attn:
-            exl2_config.no_flash_attn = True # for old devices, 2000 series and older
         self.model = ExLlamaV2(exl2_config)
         self.model.load(gpu_split)
 
@@ -391,7 +386,6 @@ class Stage1Pipeline_EXL2(Stage1Pipeline):
         seed: int,
         resume_after_n: int, # -1: don't resume, 0: after first
         extend_mp3: bool,
-        extend_mp3_start_time: int, # 0: start
         extend_mp3_end_time: int, # 0: all
         extend_current_segment: bool,
         sample_settings: SampleSettings,        
@@ -417,7 +411,7 @@ class Stage1Pipeline_EXL2(Stage1Pipeline):
         if extend_mp3:
             if vocal_track_prompt_path and instrumental_track_prompt_path:
                 print("Tokenizing mp3s")
-                existing_tokens = self.encode_existing_song_for_continuation(vocal_track_prompt_path, instrumental_track_prompt_path, extend_mp3_start_time, extend_mp3_end_time)
+                existing_tokens = self.encode_existing_song_for_continuation(vocal_track_prompt_path, instrumental_track_prompt_path, extend_mp3_end_time)
                 seq_prefix = torch.tensor([existing_tokens] * bsz, dtype=torch.long)
                 # Truncate to context limit
                 if seq_prefix.shape[-1] > max_context:
@@ -653,11 +647,9 @@ def main():
             resume_path=args.resume_path,
             cache_size=args.stage1_cache_size,
             cache_mode=args.stage1_cache_mode,
-            no_flash_attn=args.no_flash_attn,
             seed=args.seed,
             resume_after_n=args.resume_after_n,
             extend_mp3=args.extend_mp3,
-            extend_mp3_start_time=args.extend_mp3_start_time,
             extend_mp3_end_time=args.extend_mp3_end_time,
             extend_current_segment=args.extend_current_segment,
         )
@@ -683,7 +675,6 @@ def main():
         seed=args.seed,
         resume_after_n=args.resume_after_n,
         extend_mp3=args.extend_mp3,
-        extend_mp3_start_time=args.extend_mp3_start_time,
         extend_mp3_end_time=args.extend_mp3_end_time,
         extend_current_segment=args.extend_current_segment,
         run_n_segments=args.run_n_segments,
